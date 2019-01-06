ab4f2f81af9736c32b09820a53f2a6e4ab666beb
Randy L <randy@loffelmacher.com>
2019-01-06 15:16:31 -0500

    passed third test

--- bowling.go
+++ bowling.go
@@ -3,15 +3,35 @@ package main
 import "fmt"
 
 type BowlingGame struct {
-	score int
+	rolls       [21]int
+	currentRoll int
 }
 
 func (g *BowlingGame) Score() int {
-	return g.score
+	var score = 0
+	var frameIndex = 0
+	for frame := 0; frame < 10; frame++ {
+		if g.isSpare(frameIndex) {
+			score += 10 + g.rolls[frameIndex+2]
+		} else {
+			score += g.rolls[frameIndex] + g.rolls[frameIndex+1]
+		}
+		frameIndex += 2
+	}
+	return score
 }
 
 func (g *BowlingGame) Roll(pins int) {
-	g.score += pins
+	g.rolls[g.currentRoll] = pins
+	g.currentRoll++
+}
+
+func (g *BowlingGame) String() string {
+	return fmt.Sprintf("%d", g.Score())
+}
+
+func (g *BowlingGame) isSpare(frameIndex int) bool {
+	return g.rolls[frameIndex]+g.rolls[frameIndex+1] == 10
 }
 
 func main() {
--- bowling_test.go
+++ bowling_test.go
@@ -2,11 +2,15 @@ package main
 
 import "testing"
 
+func RollMany(g *BowlingGame, nn, pins int) {
+	for ii := 0; ii < nn; ii++ {
+		g.Roll(pins)
+	}
+}
+
 func TestGutterGame(t *testing.T) {
 	g := BowlingGame{}
-	for ii := 0; ii < 20; ii++ {
-		g.Roll(0)
-	}
+	RollMany(&g, 20, 0)
 	if g.Score() != 0 {
 		t.Fail()
 	}
@@ -14,10 +18,20 @@ func TestGutterGame(t *testing.T) {
 
 func TestAllOnes(t *testing.T) {
 	g := BowlingGame{}
-	for ii := 0; ii < 20; ii++ {
-		g.Roll(1)
-	}
+	RollMany(&g, 20, 1)
 	if g.Score() != 20 {
+		t.Fail()
+	}
+}
+
+func TestOneSpare(t *testing.T) {
+	g := BowlingGame{}
+	g.Roll(5)
+	g.Roll(5)
+	g.Roll(3)
+	RollMany(&g, 17, 0)
+	if g.Score() != 16 {
+		t.Log("Expected 16 but score is " + g.String())
 		t.Fail()
 	}
 }
